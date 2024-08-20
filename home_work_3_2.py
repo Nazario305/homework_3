41929d42cbdba3aadc01ece8acf692cedbedb082
git_learning <nazarzybin@gmail.com>
2024-08-20 23:57:36 +0300

    Виконання домашки номер 2 (версія 2)

--- home_work_3_2.py
+++ home_work_3_2.py
@@ -1,6 +1,17 @@
-numbers = [1, 5, 6, 8, 9]
+##v1
 
-numbers.insert(0, numbers[-1])
-numbers.pop()
-print(numbers)
+# numbers = [1, 5, 6, 8, 9]
+#
+# numbers.insert(0, numbers[-1])
+# numbers.pop()
+# print(numbers)
 
+#v2
+
+numbers = [1, 3, 25, 7, 2, 7]
+
+numbers_p = [numbers[-1]]
+numbers_min = numbers[0:-1]
+result = numbers_p + numbers_min
+
+print(result)
